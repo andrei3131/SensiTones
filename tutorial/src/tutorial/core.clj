9583b2843104dac8972151c58a71f45e8d45248a
Andrei-Octavian <andrei-octavian.brabete15@imperial.ac.uk>
2017-02-04 15:14:10 +0000

    Setup main function with basic waves

--- tutorial/src/tutorial/core.clj
+++ tutorial/src/tutorial/core.clj
@@ -2,14 +2,56 @@
 
 (use 'overtone.live)
 
-(definst foo [] (saw 299))
+(definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (sin-osc freq)
+        vol))
+(comment
+   (sin-wave)
+)
 
-(definst trem [freq 220 depth 500 rate 90 length 60]
-    (* 0.3
-       (line:kr 0 1 length FREE)
-       (saw (+ freq (* depth (sin-osc:kr rate))))))
+(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (saw freq)
+        vol))
+(comment
+   (saw-wave)
+)
+
+(definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (lf-pulse:ar freq)
+        vol))
+(comment
+   (square-wave)
+)
+
+(definst noisey [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (pink-noise) ; also have (white-noise) and others...
+        vol))
+(comment
+   (noisey)
+)
+
+(definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (lf-tri freq)
+       vol))
+(comment
+   (triangle-wave)
+)
+
+(definst spooky-house [freq 440 width 0.2 attack 0.3 sustain 4 release 0.3 vol 0.4] 
+    (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+       (sin-osc (+ freq (* 20 (lf-pulse:kr 0.5 0 width))))
+        vol))
+(comment
+  (spooky-house)  
+)
 
 (defn -main
-    "Sentient Note"
-    []
-    (foo))
+      "Sentient Note"
+      []
+      (saw-wave))
+
--- tutorial/src/tutorial/mytest.clj
+++
@@ -1,8 +0,0 @@
-(definst foo [] (saw 299))
-
-(definst trem [freq 220 depth 500 rate 90 length 60]
-    (* 0.3
-       (line:kr 0 1 length FREE)
-       (saw (+ freq (* depth (sin-osc:kr rate))))))
-
-(foo)
