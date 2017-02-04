679c8d37e046feff7cefb955347864dcb77216d6
ad5915 <dan.alex97@yahoo.com>
2017-02-04 19:51:10 +0000

    Finised connecting back end with the clojure sources. Also modified the mechanism for requests.

--- cgi/engine.py
+++ cgi/engine.py
@@ -1,7 +1,19 @@
-import sentiment as sen
+from sentiment import post_request
 
-print "(stop)"
+class Engine:
+	def __init__(self):
+		self.message_vector = ""
+		self.max_tot_length = 40
+		self.path = "../tutorial/"
+	def process_message(self, mess):
+		self.message_vector += mess		
+		if len(self.message_vector) > self.max_tot_length:
+			sen = post_request(self.message_vector)
 
-sentiment = sen.post_request('Hello')
-if sentiment > 0.5:
-        print "(demo (sin-osc))"
+			sen_norm = int(100 * sen)
+
+			fo = open(self.path + "sentimentalert", "wb")
+			fo.write(str(sen_norm))
+			fo.close()
+
+			self.message_vector = ""
--- cgi/sentiment.py
+++ cgi/sentiment.py
@@ -1,17 +1,15 @@
 import subprocess
 
-path = "/home/andrei-octavian/Hack/ICHack/cgi/"
-
 def post_request_string(str):
     """
     Makes a request to get sentiment for a single string.
     """
 
-    fo = open(path + "reqbuffer.txt", "wb")
+    fo = open("reqbuffer.txt", "wb")
     fo.write(str)
     fo.close()
 
-    request_script = subprocess.Popen(['ruby', path + 'req.rb'],
+    request_script = subprocess.Popen(['ruby', 'req.rb'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
     out, err = request_script.communicate()
