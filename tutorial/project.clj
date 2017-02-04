d939fca18f7e7c501b9b6b6cdb25ed950cf8a870
ad5915 <dan.alex97@yahoo.com>
2017-02-04 11:26:09 +0000

    Added a sample example from ctford's repository.

--- tutorial/project.clj
+++ tutorial/project.clj
@@ -1,3 +1,5 @@
 (defproject tutorial "1.0"
   :dependencies [ [org.clojure/clojure "1.8.0"]
-                  [overtone "0.10.1"] ])
+                  [overtone "0.10.1"]
+                  [leipzig "0.10.0"]
+                  [quil "2.5.0"] ])
