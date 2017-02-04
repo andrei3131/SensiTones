c1d005c8beac51e9dca0c000917ec9f2add3f903
Ioan Budea <ioanbudea@dyn1218-234.wlan.ic.ac.uk>
2017-02-04 22:51:58 +0200

    Multiple chats and user details included.

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,42 @@
 var chatApp = angular.module('chatApp', []);
 
-chatApp.controller('mainController', function($scope) {
+chatApp.controller('mainController', function($scope, $timeout) {
     $scope.messages = [{time: "2017-02-04T19:10:21.932Z", message: "Hi, how are you?"},
     {time: "2017-02-04T19:12:21.932Z", message: "Good, you?"},
     {time: "2017-02-04T19:14:21.932Z", message: "Good"}];
 
+    $scope.messages2 = [];
+
+    
+    $scope.users = [{name: "Alex", dob: "1995-02-04T19:10:21.93", location: "London, UK", messages: $scope.messages}, 
+    {name: "Jane", dob: "1994-05-07T19:10:21.93", location: "Manchester, UK", messages: $scope.messages2}];
+    
+    $scope.currentChatUser = $scope.users[0];
     
     $scope.post_mess = function() {
         
-        $scope.newPost = {};
-        $scope.newPost.time = new Date();
-        $scope.newPost.message = $scope.myMessage;
-        
-        console.log($scope.newPost);
-        
-        $scope.messages.push($scope.newPost);
-        $scope.myMessage = "";
+        if($scope.myMessage != "") {
+            $scope.newPost = {};
+            $scope.newPost.time = new Date();
+            $scope.newPost.message = $scope.myMessage;
+            
+            console.log($scope.newPost);
+            
+            $scope.currentChatUser.messages.push($scope.newPost);
+            $scope.myMessage = "";
         //$.post("http://127.0.0.1:3600", x);
+        }
+        
+        $timeout($scope.updateScroll, 100);
     }
+    
+    $scope.updateScroll = function() {
+        var element = document.getElementById("scrollable");
+        element.scrollTop = element.scrollHeight;
+    }
+    
+    $scope.changeChat = function(user) {
+        $scope.currentChatUser = user;
+    }
+    
 });
