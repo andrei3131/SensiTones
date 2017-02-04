d9b43ad937d99c1971d142ac33c2e4147c1ce9c3
ad5915 <dan.alex97@yahoo.com>
2017-02-04 15:41:44 +0000

    The requests to the server are now working and the sentiments are captured.

--- cgi/sentiment.py
+++ cgi/sentiment.py
@@ -1,26 +1,15 @@
-import httplib, urllib, base64
+import subprocess
 
-headers = {
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '7911c204a38749acba19d26e6bb2e763',
-}
+def post_request_string(str):
+    """
+    Makes a request to get sentiment for a single string.
+    """
+    
+    request_script = subprocess.Popen(['ruby', 'req.rb'], 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
+    out, err = request_script.communicate()
+ 
+    return out
 
-params = urllib.urlencode('{\
-  "documents": [\
-    {\
-      "language": "string",\
-      "id": "string",\
-      "text": "string"\
-    }\
-  ]\
-}')
-
-try:
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+print post_request_string("hi")
