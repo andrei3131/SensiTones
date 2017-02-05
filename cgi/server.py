13977218550ba8c42c1c0f442c5190e83e2c8461
ad5915 <dan.alex97@yahoo.com>
2017-02-05 09:40:58 +0000

    Solved wrong ports connection issue.

--- cgi/server.py
+++ cgi/server.py
@@ -2,6 +2,7 @@ import socket
 import re
 
 import sys
+from subprocess import call
 
 from engine import Engine
 
@@ -12,30 +13,59 @@ else:
 	port = int(sys.argv[1])
 print "Server starting on port " + str(port)
 
-host = '' 
+################################################################################
+
+my_file = '../frontend/script.js'
+
+line_good = """$.post("http://127.0.0.1:""" + str(port) + """ ", $scope.newPost);"""
+line_nbr = 27
+
+content = ""
+with open(my_file, 'r') as content_file:
+	content = content_file.read()
+	content_file.close()
+
+cnt = 0
+output = ""
+for line in content.split('\n'):
+	cnt += 1
+	if cnt == line_nbr:
+	 	output += line_good
+	else:
+		output += line
+	output += '\n'
+print output
+
+fo = open(my_file, "wb")
+fo.write(output)
+fo.close()
+
+################################################################################
+
+host = ''
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
-sock.listen(1) 
+sock.listen(1)
 
 engine = Engine()
 
 while True:
     csock, caddr = sock.accept()
     print "Connection from: " + `caddr`
-    req = csock.recv(1024) 
-    
+    req = csock.recv(1024)
+
     lines = req.split('\n')
-    
+
     message = ""
     arrived = False
     for line in lines:
     	if line == "\r":
     		arrived = True
-    	line += '\n' 
+    	line += '\n'
     	if arrived:
     		message += line
 
-    engine.process_message(message) 
+    engine.process_message(message)
 
     csock.sendall("""HTTP/1.0 200 OK
 		Content-Type: text/html
