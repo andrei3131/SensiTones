a499a89c1fa42be1ca1a89648f409c2d1547c01f
Andrei-Octavian <andrei-octavian.brabete15@imperial.ac.uk>
2017-02-05 11:48:29 +0000

    Fixed the last problems at song persing at change of song.

--- tutorial/trans.py
+++ tutorial/trans.py
@@ -1,71 +1,84 @@
-content = ""
+def execu():
+    content = ""
 
-with open('input.txt', 'r') as content_file:
-    content = content_file.read()
+    with open('input.txt', 'r') as content_file:
+        content = content_file.read()
 
-map = {
-'C' : 0,
-'D' : 1,
-'E' : 2,
-'F' : 3,
-'G' : 4,
-'A' : 5,
-'B' : 6
-}
+    map = {
+    'C' : 0,
+    'D' : 1,
+    'E' : 2,
+    'F' : 3,
+    'G' : 4,
+    'A' : 5,
+    'B' : 6
+    }
 
-#############################
+    #############################
 
-spans = content.split("</span>")
-words_notes = [x.split(">") for x in spans]
+    spans = content.split("</span>")
+    words_notes = [x.split(">") for x in spans]
 
-words = []
-notes = []
-for xs in words_notes:
-    if len(xs) == 1:
-        continue
+    words = []
+    notes = []
+    for xs in words_notes:
+        if len(xs) == 1:
+            continue
 
-    raw_words = xs[0]
-    note = xs[1]
+        raw_words = xs[0]
+        note = xs[1]
 
-    ys = raw_words.split(' ')
+        ys = raw_words.split(' ')
 
-    notes.append(note)
-    cnt = 0
-    for y in ys:
-        if y.isalnum():
-            cnt += 1
-
-    words.append(cnt)
-
-cnt = 0
-tot = 0
-durations = []
-for pos in range(len(notes)):
-    w = words[pos]
-
-    tot += w
-    cnt += 1
-    if w != 0:
-        length = tot/cnt
-        for i in range(cnt):
-            durations.append("1/" + str(max(length, 1) + 1))
-        tot = 0
+        notes.append(note)
         cnt = 0
+        for y in ys:
+            if y.isalnum():
+                cnt += 1
 
-last = len(notes) - 1
-while words[last] == 0:
-    last -= 1
+        words.append(cnt)
 
-notes = [x for x in notes[0:last+1]]
-notes = [str(x[0]) for x in notes]
-notes = [map[x] for x in notes]
+    cnt = 0
+    tot = 0
+    durations = []
+    for pos in range(len(notes)):
+        w = words[pos]
+
+        tot += w
+        cnt += 1
+        if w != 0:
+            length = tot/cnt
+            for i in range(cnt):
+                durations.append("1/" + str(max(length, 1) + 1))
+            tot = 0
+            cnt = 0
+    out = ""
+
+    last = len(notes) - 1
+    while words[last] == 0:
+        last -= 1
+
+    notes = [x for x in notes[0:last+1]]
+    notes = [str(x[0]) for x in notes]
+    notes = [map[x] for x in notes]
+
+
+    out = append_array(out, notes)
+    out += '\n'
+    out = append_array(out, durations)
+
+    fo = open("notes.txt", "wb")
+    fo.write(out)
+    fo.close()
+
+
+    return notes, durations
 
 # print notes
 # print durations
 
 #############################
 
-out = ""
 
 def append_array(out, arr):
     out += '['
@@ -75,15 +88,11 @@ def append_array(out, arr):
     return out
 
 def get_notes():
+    notes, durations = execu()
     return append_array("", notes)
 
 def get_durations():
+    notes, durations = execu()
     return append_array("", durations)
 
-out = append_array(out, notes)
-out += '\n'
-out = append_array(out, durations)
-
-fo = open("notes.txt", "wb")
-fo.write(out)
-fo.close()
+execu()
